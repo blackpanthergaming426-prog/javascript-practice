0ff3840fa9dca119279f0c60507fedaad214f339
blackpanthergaming426-prog <blackpanthergaming426@gmail.com>
2025-11-10 23:54:04 +0500

    lecture-3-completd

--- javascript-lecture-3/index.js
+++ javascript-lecture-3/index.js
@@ -6,24 +6,32 @@ let car=null;
 let bignumber=12345679101112134151617;
 let id1=Symbol('id')
 console.log( typeof name,typeof age,typeof isonline,typeof landcruiser,typeof car,typeof bignumber,typeof id1); */
-let name = 'waqar';
-let age = 18;
-let isonline = true;
-let landcruiser;     
-let car = null;
-let bignumber = 12345679101112134151617n; // bigint ke liye 'n' zaroori hai
-let id1 = Symbol('id');
+let name = 'waqar';  //string
+let age = 18;        //number
+let isonline = true; //boolean
+let landcruiser;     //undefined
+let car = null;      //object
+let bignumber = 12345679101112134151617n; // bigint
+let id1 = Symbol('id');  //symbol
+
+//Copy Test
+ let num1 = 10
+let num2=num1
+num2 = 20
+//javaScript me agar ek number ko dusre variable me copy kiya jaye, to wo value ki copy hoti hai, isliye dusre variable ko change karne se asli number par koi asar nahi padta.
 
 console.log(
   `name: ${name}  (${typeof name}), ` +
   `age: ${age} (${typeof age}), ` +
   `isonline: ${isonline} (${typeof isonline}) ` +
   `landcruiser: ${landcruiser} (${typeof landcruiser}), ` +
-   `car: ${car} (${typeof car}), `+
+  `car: ${car} (${typeof car}), ` +
   `bignumber: ${bignumber} (${typeof bignumber}), ` +
-  `id1: ${id1.toString()} (${typeof id1})` 
-);
+  `id1: ${id1.toString()} (${typeof id1}), ` +
+  `num1: ${num1},num2: ${num2}`
+ );
 
 
- 
-    
+
+
+
--- javascript-lecture-3/newFile.js
+++ javascript-lecture-3/newFile.js
@@ -1,1 +1,42 @@
+/* //primitive data type
+let name="waqar"
+let age=18
+
+//Non-primitive data type
+let user=["waqar","18",]
+let people= { name:"waqar", age:"22"}
+console.log(name,age ,typeof name,typeof age,typeof user,typeof people); */
+/* console.log("Above output will be cleared now...");
+console.clear(); */
+/* let username = prompt("enter your name");
+let userage = prompt("enter your age");
+let isstudent = prompt("Are you a student? (true/false)");
+let convertisstudent = Boolean(isstudent)
+let favNumber = prompt("Enter your favorite number");
+let convertedfavNumber = number(favNumber)
+let nickName = prompt("Enter your nickname");
+console.log(
+  username, typeof username,
+  userage, typeof userage,
+  isstudent, typeof isstudent,
+  favNumber, typeof favNumber,
+  nickName, typeof nickName
+); */
+let username = prompt("Enter your name");
+
+let userage = Number(prompt("Enter your age")); // direct number conversion
+
+let isstudent = prompt("Are you a student? (true/false)").toLowerCase() === "true"; // direct boolean conversion
+
+let favNumber = Number(prompt("Enter your favorite number")); // direct number conversion
+
+let nickName = prompt("Enter your nickname");
+
+console.log(
+  username, typeof username,
+  userage, typeof userage,
+  isstudent, typeof isstudent,
+  favNumber, typeof favNumber,
+  nickName, typeof nickName
+);
 

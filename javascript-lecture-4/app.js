3a680982df3e063e04d0149e271864f0b58a4bbf
blackpanthergaming426-prog <blackpanthergaming426@gmail.com>
2025-11-11 03:27:37 +0500

    lecture 4 completed

---
+++ javascript-lecture-4/app.js
@@ -0,0 +1,19 @@
+let num1= Number(prompt("enter your 1st number"));
+let num2= Number(prompt("enter your 2nd number"));
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(num1**num2);
+let savedemail='waqar@gmail.com';
+let savedpassword='haker125'
+ let email=prompt("enter your email adress");
+let password=(prompt("enter your password")); //if type number answer flase */
+console.log(email===savedemail && password==savedpassword); 
+ 
+let counter=22;
+counter++
+console.log(counter);
+
+
---
+++ javascript-lecture-4/counter.js
@@ -0,0 +1,14 @@
+/* let counter = 0;
+console.log(counter++);
+console.log(counter--);
+console.log(++counter);
+console.log(--counter);
+console.log(counter++); */
+let marks=Number(prompt("give me marks 1 out of 100"));
+console.log("pass", marks >=50);
+console.log("high-score",marks===100);
+
+
+
+
+
